6abc6ccd1fb2ef32600cb0d2d99e358945cd9c67
LupinThe1st <1846233017@qq.com>
2023-12-03 19:42:58 +0800

    Kmall后台管理项目完成

---
+++ src/views/404/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <img style="width: 666px;" src="../../assets/images/error_images/404.png" alt="" />
+  <el-button
+    @click="goHome"
+    style="position: absolute; top: 0; left: 0; margin: 10px"
+    type="primary"
+  >
+    首页
+  </el-button>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+let $router = useRouter()
+const goHome = () => {
+  $router.push('/home')
+}
+</script>
+
+<style scoped lang="scss">
+img {
+  display: block;
+  margin: 0 auto;
+}
+</style>
